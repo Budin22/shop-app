a702e7631dab33210f8de566510935333dccd8ae
Sergii <apserbud22@gmail.com>
2022-12-01 11:16:55 +0200

    change repo

--- server/src/config/keys.ts
+++
@@ -1,3 +0,0 @@
-export const jwtSecret = "21d2fg1dfg1d1grr4utyiuty51ws3df4sd4gfsdf-sdfs5e5e";
-
-export const sessionKey = "22";
--- server/src/utils/errorHandler.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from "express";
-
-export const errorHandler = (res: Response, error: any) => {
-  res
-    .status(500)
-    .json({ success: false, msg: error.message ? error.message : error });
-};
