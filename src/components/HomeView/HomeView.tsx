cb91b9ba74245fe9b22a9e4f7890ef07509b012a
Sergii <apserbud22@gmail.com>
2026-01-27 10:33:27 +0200

    added iframe

--- src/components/HomeView/HomeView.tsx
+++ src/components/HomeView/HomeView.tsx
@@ -7,6 +7,14 @@ export const HomeView = memo(() => {
       <Typography gutterBottom variant="h1" component="h1" textAlign="center">
         Hello from home page
       </Typography>
+        <iframe
+            src="https://stage-bcr.infuse.com/"
+            width="900"
+            height="785"
+            title="Iframe Example"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+        ></iframe>
     </>
   );
 });
